6129894c44641bac1d3c772408812152139e3229
Samaila Ayuba <samailaayuba61@gmail.com>
2023-09-09 06:54:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 /*Hello puts- my first c program*/
 #include <stdio.h>
 
-int main(){
+int main(void
+	){
 
 	puts("Programming is like building a multilingual puzzle);
 
-	return 0;
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 main()
 	{
-	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	printf("with proper grammar, but the outcome is a piece of art,"\r\n);
 
 		return (0);
 	}
