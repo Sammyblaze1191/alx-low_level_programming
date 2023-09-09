291caab14440c7bff3048616fbdd8541b6744294
Samaila Ayuba <samailaayuba61@gmail.com>
2023-09-09 07:00:26 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 /*Hello puts- my first c program*/
 #include <stdio.h>
 
-int main(void
-	){
+main()
+{
 
-	puts("Programming is like building a multilingual puzzle);
+	puts("Programming is like building a multilingual puzzle\n);
 
 	return (0);
 }
