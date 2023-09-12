4ebc10c1558c7892df081960ada1b3b6731f0ef7
Samaila Ayuba <samailaayuba61@gmail.com>
2023-09-12 15:22:33 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*main-print _putchar
+ *
+ *return: 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('a');
+	putchar('b');
+	putchar('c');
+	putchar('z');
+	putchar('\n');
+	return (0);
+}
